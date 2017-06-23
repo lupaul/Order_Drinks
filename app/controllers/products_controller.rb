ea0837309001a2a6b047a3a1426fe99e716bb6aa
pochia <lupochia@gmail.com>
2017-06-23 17:12:17 +0800

    add product to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,11 +15,26 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       format.js
-      
     end
   end
 
   def add_to_cart
-    #code
+    @cartitem = current_cart.cart_items.new(item_params)
+    @cartitem.product_id = params[:proid]
+
+    if !current_cart.checkitem?(params[:proid], @cartitem)
+      @cartitem.save
+      redirect_to root_path
+      flash[:notice] = "成功加入訂單！！"
+    else
+      redirect_to root_path
+      flash[:alert] = "出現錯誤! 請重新選取!!"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:venti_quantity, :grande_quantity)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,9 +2,13 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items, source: :product
 
-  def add_product_to_cart(product)
-    ci = cart_items.build
-    ci.product = product
-    ci.save
+  def checkitem?(proid, productqty)
+    sum = productqty.venti_quantity + productqty.grande_quantity
+    if sum == 0
+      return true
+    end
+    if items.find_by(id: proid).present?
+      return true
+    end
   end
 end
