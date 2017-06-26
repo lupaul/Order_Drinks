505eb5ac3448304c33509a31011322cd4cab97d2
pochia <lupochia@gmail.com>
2017-06-26 13:11:29 +0800

    add product rspec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price_venti, presence: true
+  validates :price_grande, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with valid attributes" do
+    product = Product.new(name: "Anything", price_venti: 35, price_grande: 30)
+
+    expect(product).to be_valid
+  end
+
+  it "is valid without name" do
+    product = Product.new(name: nil, price_venti: 35, price_grande: 30)
+    product.valid?
+    expect(product.errors[:name]).to include("can't be blank")
+  end
+
+  it "is valid without price_venti" do
+    product = Product.new(name: "Greentea", price_venti: nil, price_grande: 30)
+    product.valid?
+    expect(product.errors[:price_venti]).to include("can't be blank")
+  end
+
+  it "is valid without price_grande" do
+    product = Product.new(name: "Blacktea", price_venti: 35, price_grande: nil)
+    product.valid?
+    expect(product.errors[:price_grande]).to include("can't be blank")
+  end
+
 end
